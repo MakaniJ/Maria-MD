81468b6a9491a2851252627831cf71b39e7c80a8
MakaniJ <159890825+MakaniJ@users.noreply.github.com>
2024-02-13 23:05:17 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownername = "💙makani"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
